ea185c32d97dfffd464c6c6632c5455813dc34ea
EmilQlz <58591330+EmilQlz@users.noreply.github.com>
2021-12-10 10:52:31 +0400

    firstUpload

---
+++ Assets/_Main/3.Scripts/2.Managers/GameAnalyticsScript.cs
@@ -0,0 +1,11 @@
+using GameAnalyticsSDK;
+using UnityEngine;
+
+public class GameAnalyticsScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameAnalytics.Initialize();
+    }
+}
